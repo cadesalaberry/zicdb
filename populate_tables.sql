e3ea097dc71600fb4474248e4a6e0010cb19a6a9
Sean Finkel <seandavidfinkel@gmail.com>
2014-03-27 19:22:10 -0400

    Bug: syntax error at or near 'user_likes_playlist'

--- populate_tables.sql
+++ populate_tables.sql
@@ -1,33 +1,33 @@
--- Artist 1 - Metallica
+﻿-- Artist 1 - Metallica
 
 INSERT INTO cs421g22.artists VALUES (1, 'Metallica', 'www.feelthedeath.com');
 INSERT INTO cs421g22.collections VALUES (1, 'Cribs on Duty', 'Death Metal', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (1,1980, 3, 'This is a good album');
 INSERT INTO cs421g22.links VALUES (1, 'www.songs.com/3123', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (1, 'Death baby', 120);
-INSERT INTO cs421g22.link_to_song VALUES (1, 1);
+INSERT INTO cs421g22.song_has_link VALUES (1, 1);
 INSERT INTO cs421g22.song_by_artist VALUES (1, 1);
 INSERT INTO cs421g22.song_in_album VALUES (1, 1);
 INSERT INTO cs421g22.links VALUES (2, 'www.songs.com/314424', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (2, 'Kill the nanny', 155);
-INSERT INTO cs421g22.link_to_song VALUES (2, 2);
+INSERT INTO cs421g22.song_has_link VALUES (2, 2);
 INSERT INTO cs421g22.song_by_artist VALUES (2, 1);
 INSERT INTO cs421g22.song_in_album VALUES (2, 1);
 INSERT INTO cs421g22.links VALUES (3, 'www.greatsongs.com/3aab', 'greatsongs.com');
 INSERT INTO cs421g22.songs VALUES (3, 'God is a Satan!', 150);
-INSERT INTO cs421g22.link_to_song VALUES (3, 3);
+INSERT INTO cs421g22.song_has_link VALUES (3, 3);
 INSERT INTO cs421g22.song_by_artist VALUES (3, 1);
 INSERT INTO cs421g22.song_in_album VALUES (3, 1);
 INSERT INTO cs421g22.collections VALUES (2, 'Cops Kill', 'Death Metal', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (2,2000, 2, 'This album is not good :(');
 INSERT INTO cs421g22.links VALUES (4, 'www.songs.com/33', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (4, 'Death cop', 200);
-INSERT INTO cs421g22.link_to_song VALUES (4, 4);
+INSERT INTO cs421g22.song_has_link VALUES (4, 4);
 INSERT INTO cs421g22.song_by_artist VALUES (4, 1);
 INSERT INTO cs421g22.song_in_album VALUES (4, 2);
 INSERT INTO cs421g22.links VALUES (5, 'www.oksongs.com/324', 'oksongs.com');
 INSERT INTO cs421g22.songs VALUES (5, 'Cops are bad lol', 300);
-INSERT INTO cs421g22.link_to_song VALUES (5, 5);
+INSERT INTO cs421g22.song_has_link VALUES (5, 5);
 INSERT INTO cs421g22.song_by_artist VALUES (5, 1);
 INSERT INTO cs421g22.song_in_album VALUES (5, 2);
 
@@ -40,29 +40,29 @@ INSERT INTO cs421g22.collections VALUES (3, 'I want to be your baby', 'Pop', '20
 INSERT INTO cs421g22.albums VALUES (3,1999, 4, 'This is a shitty album');
 INSERT INTO cs421g22.links VALUES (6, 'www.songs.com/312abbb', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (6, 'Baby baby', 123);
-INSERT INTO cs421g22.link_to_song VALUES (6, 6);
+INSERT INTO cs421g22.song_has_link VALUES (6, 6);
 INSERT INTO cs421g22.song_by_artist VALUES (6, 2);
 INSERT INTO cs421g22.song_in_album VALUES (6, 3);
 INSERT INTO cs421g22.links VALUES (7, 'www.songs.com/314424934', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (7, 'Baby whats ur twitter name', 215);
-INSERT INTO cs421g22.link_to_song VALUES (7, 7);
+INSERT INTO cs421g22.song_has_link VALUES (7, 7);
 INSERT INTO cs421g22.song_by_artist VALUES (7, 2);
 INSERT INTO cs421g22.song_in_album VALUES (7, 3);
 INSERT INTO cs421g22.links VALUES (8, 'www.greatamazingsongs.com/wr234fny', 'greatamazingsongs.com');
 INSERT INTO cs421g22.songs VALUES (8, 'Where do you store your keys', 250);
-INSERT INTO cs421g22.link_to_song VALUES (8, 8);
+INSERT INTO cs421g22.song_has_link VALUES (8, 8);
 INSERT INTO cs421g22.song_by_artist VALUES (8, 2);
 INSERT INTO cs421g22.song_in_album VALUES (8, 3);
 INSERT INTO cs421g22.links VALUES (9, 'www.songsilike.com/asjb59d', 'songsilike.com');
 INSERT INTO cs421g22.songs VALUES (9, 'Baby give me your knees', 1525);
-INSERT INTO cs421g22.link_to_song VALUES (9, 9);
+INSERT INTO cs421g22.song_has_link VALUES (9, 9);
 INSERT INTO cs421g22.song_by_artist VALUES (9, 2);
 INSERT INTO cs421g22.song_in_album VALUES (9, 3);
 INSERT INTO cs421g22.collections VALUES (4, 'Adult Time Bitch', 'Rap', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (4,2000, 1, 'This album is nooooot good :(');
 INSERT INTO cs421g22.links VALUES (10, 'www.songs.com/1ft10g', 'songs.com');
 INSERT INTO cs421g22.songs VALUES (10, 'I am now a rap friend', 217);
-INSERT INTO cs421g22.link_to_song VALUES (10, 10);
+INSERT INTO cs421g22.song_has_link VALUES (10, 10);
 INSERT INTO cs421g22.song_by_artist VALUES (10, 2);
 INSERT INTO cs421g22.song_in_album VALUES (10,4);
 
@@ -74,12 +74,12 @@ INSERT INTO cs421g22.collections VALUES (5, 'Rap is my game', 'Rap', '2013-10-09
 INSERT INTO cs421g22.albums VALUES (5,1991, 2, 'This is very much rap');
 INSERT INTO cs421g22.songs VALUES (11, 'Baby I rap', 301);
 INSERT INTO cs421g22.links VALUES (11, 'www.songs.com/32f31s', 'songs.com');
-INSERT INTO cs421g22.link_to_song VALUES (11, 11);
+INSERT INTO cs421g22.song_has_link VALUES (11, 11);
 INSERT INTO cs421g22.song_by_artist VALUES (11, 3);
 INSERT INTO cs421g22.song_in_album VALUES (11, 5);
 INSERT INTO cs421g22.songs VALUES (12, 'Baby I rap on your face', 300);
 INSERT INTO cs421g22.links VALUES (12, 'www.supasongs.com/asd2ttf', 'supasongs.com');
-INSERT INTO cs421g22.link_to_song VALUES (12, 12);
+INSERT INTO cs421g22.song_has_link VALUES (12, 12);
 INSERT INTO cs421g22.song_by_artist VALUES (12, 3);
 INSERT INTO cs421g22.song_in_album VALUES (12, 5);
 
@@ -92,26 +92,26 @@ INSERT INTO cs421g22.collections VALUES (6, 'Miss Marm', 'Rock', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (6,1972, 2, 'This is the worst album');
 INSERT INTO cs421g22.songs VALUES (13, 'Miss Marm', 23);
 INSERT INTO cs421g22.links VALUES (13, 'www.songs.com/2d24g', 'songs.com');
-INSERT INTO cs421g22.link_to_song VALUES (13, 13);
+INSERT INTO cs421g22.song_has_link VALUES (13, 13);
 INSERT INTO cs421g22.song_by_artist VALUES (13, 4);
 INSERT INTO cs421g22.song_in_album VALUES (13, 6);
 INSERT INTO cs421g22.songs VALUES (14, 'Envy Pig', 15);
 INSERT INTO cs421g22.links VALUES (14, 'www.gsongs.com/as3g3d', 'gsongs.com');
-INSERT INTO cs421g22.link_to_song VALUES (14, 14);
+INSERT INTO cs421g22.song_has_link VALUES (14, 14);
 INSERT INTO cs421g22.song_by_artist VALUES (14, 4);
 INSERT INTO cs421g22.song_in_album VALUES (14,6);
 INSERT INTO cs421g22.collections VALUES (7, 'Janet was a b', 'Pop', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (7,2010, 1, 'Janet Why :(');
 INSERT INTO cs421g22.songs VALUES (15, 'I am songs', 627);
 INSERT INTO cs421g22.links VALUES (15, 'www.greatsongs.com/1ft152g', 'greatsongs.com');
-INSERT INTO cs421g22.link_to_song VALUES (15, 15);
+INSERT INTO cs421g22.song_has_link VALUES (15, 15);
 INSERT INTO cs421g22.song_by_artist VALUES (15, 4);
 INSERT INTO cs421g22.song_in_album VALUES (15,7);
 INSERT INTO cs421g22.collections VALUES (8, 'Horizons Bokoko', 'Rock', '2013-10-09');
 INSERT INTO cs421g22.albums VALUES (8,2014,1, 'This album igood :(');
 INSERT INTO cs421g22.songs VALUES (16, 'I am the best', 216);
 INSERT INTO cs421g22.links VALUES (16, 'www.greatsongs.com/1ft163g', 'greatsongs.com');
-INSERT INTO cs421g22.link_to_song VALUES (16, 16);
+INSERT INTO cs421g22.song_has_link VALUES (16, 16);
 INSERT INTO cs421g22.song_by_artist VALUES (16, 4);
 INSERT INTO cs421g22.song_in_album VALUES (16,8);
 
@@ -124,12 +124,12 @@ INSERT INTO cs421g22.collections VALUES (9, 'La Vie En Pourpre', 'Pop', '2013-10
 INSERT INTO cs421g22.albums VALUES (9,1950, 2, 'Ceci est tres bon album! Oui Oui france!');
 INSERT INTO cs421g22.songs VALUES (17, 'Madame Marm', 211);
 INSERT INTO cs421g22.links VALUES (17, 'www.songs.com/2d209t4g', 'songs.com');
-INSERT INTO cs421g22.link_to_song VALUES (17, 17);
+INSERT INTO cs421g22.song_has_link VALUES (17, 17);
 INSERT INTO cs421g22.song_by_artist VALUES (17, 5);
 INSERT INTO cs421g22.song_in_album VALUES (17,9);
 INSERT INTO cs421g22.songs VALUES (18, 'Envy Pig', 600);
 INSERT INTO cs421g22.links VALUES (18, 'www.greatsongs.com/asd2g3', 'greatsongs.com');
-INSERT INTO cs421g22.link_to_song VALUES (18, 18);
+INSERT INTO cs421g22.song_has_link VALUES (18, 18);
 INSERT INTO cs421g22.song_by_artist VALUES (18, 5);
 INSERT INTO cs421g22.song_in_album VALUES (18,9);
 
@@ -155,7 +155,7 @@ INSERT INTO cs421g22.song_in_playlist VALUES (1,10);
 INSERT INTO cs421g22.song_in_playlist VALUES (6,10);
 INSERT INTO cs421g22.song_in_playlist VALUES (10,10);
 INSERT INTO cs421g22.song_in_playlist VALUES (11,10);                                                                                                                                                                                                                   
-INSERT INTO cs421g22.user_likes_playlist VALUES (10, 0);
+INSERT INTO cs421g22.user_likes_playlist VALUES (0, 10);
 
 INSERT INTO cs421g22.collections VALUES (11, 'La Vie Est Pas Belle', 'Rock', '2011-09-10');
 INSERT INTO cs421g22.playlists VALUES (11,2);
@@ -163,20 +163,20 @@ INSERT INTO cs421g22.song_in_playlist VALUES (1,11);
 INSERT INTO cs421g22.song_in_playlist VALUES (5,11);
 INSERT INTO cs421g22.song_in_playlist VALUES (12,11);
 INSERT INTO cs421g22.song_in_playlist VALUES (4,11);       
-INSERT INTO cs421g22.user_likes_playlist VALUES (11, 3);
-INSERT INTO cs421g22.user_likes_playlist VALUES (11, 1);
+INSERT INTO cs421g22.user_likes_playlist VALUES (3, 11);
+INSERT INTO cs421g22.user_likes_playlist VALUES (1, 11);
 
 INSERT INTO cs421g22.collections VALUES (12, 'Belle Vie, Yo Pute', 'Rap', '2012-09-09');
 INSERT INTO cs421g22.playlists VALUES (12,3);
 INSERT INTO cs421g22.song_in_playlist VALUES (4,12); 
-INSERT INTO cs421g22.user_likes_playlist VALUES (12, 0);
-INSERT INTO cs421g22.user_likes_playlist VALUES (12, 1);
-INSERT INTO cs421g22.user_likes_playlist VALUES (12, 3);
+INSERT INTO cs421g22.user_likes_playlist VALUES (0, 12);
+INSERT INTO cs421g22.user_likes_playlist VALUES (1, 12);
+INSERT INTO cs421g22.user_likes_playlist VALUES (3, 12);
 
 INSERT INTO cs421g22.collections VALUES (13, 'Belle Vie, Yo Pute II', 'Rap', '2012-09-13');
 INSERT INTO cs421g22.playlists VALUES (13,3);
 INSERT INTO cs421g22.song_in_playlist VALUES (7,13);
-INSERT INTO cs421g22.user_likes_playlist VALUES (13, 3);
+INSERT INTO cs421g22.user_likes_playlist VALUES (3, 13);
 
 
 
@@ -194,9 +194,9 @@ INSERT INTO cs421g22.song_in_playlist VALUES (12,14);
 INSERT INTO cs421g22.song_in_playlist VALUES (14,14);
 INSERT INTO cs421g22.song_in_playlist VALUES (3,14);
 INSERT INTO cs421g22.song_in_playlist VALUES (7,14);                                                                                                                                                                                                                   
-INSERT INTO cs421g22.user_likes_playlist VALUES (14, 2);
-INSERT INTO cs421g22.user_likes_playlist VALUES (14, 0);
-INSERT INTO cs421g22.user_likes_playlist VALUES (14, 3);
+INSERT INTO cs421g22.user_likes_playlist VALUES (2, 14);
+INSERT INTO cs421g22.user_likes_playlist VALUES (0, 14);
+INSERT INTO cs421g22.user_likes_playlist VALUES (3, 14);
 
 
 
@@ -210,7 +210,7 @@ INSERT INTO cs421g22.song_in_playlist VALUES (1,15);
 INSERT INTO cs421g22.song_in_playlist VALUES (2,15);
 INSERT INTO cs421g22.song_in_playlist VALUES (4,15);
 INSERT INTO cs421g22.song_in_playlist VALUES (9,15);                                                                                                                                                                                                                   
-INSERT INTO cs421g22.user_likes_playlist VALUES (15, 0);
+INSERT INTO cs421g22.user_likes_playlist VALUES (0, 15);
 
 INSERT INTO cs421g22.collections VALUES (16, 'I am piss piss', 'Rap', '2014-06-10');
 INSERT INTO cs421g22.playlists VALUES (16,2);
@@ -218,8 +218,8 @@ INSERT INTO cs421g22.song_in_playlist VALUES (1,16);
 INSERT INTO cs421g22.song_in_playlist VALUES (3,16);
 INSERT INTO cs421g22.song_in_playlist VALUES (5,16);
 INSERT INTO cs421g22.song_in_playlist VALUES (7,16);       
-INSERT INTO cs421g22.user_likes_playlist VALUES (16, 3);
-INSERT INTO cs421g22.user_likes_playlist VALUES (16, 1);
+INSERT INTO cs421g22.user_likes_playlist VALUES (3, 16);
+INSERT INTO cs421g22.user_likes_playlist VALUES (1, 16);
 
 
 
